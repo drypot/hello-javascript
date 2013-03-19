deba92ff8db0f2567e44a676d086854358068768
drypot <drypot@gmail.com>
2013-03-19 10:33:05 +0900

    console.log to assert in array.js.

--- javascript-abc/array.js
+++ javascript-abc/array.js
@@ -1,76 +1,102 @@
-// creating
+var assert = require('assert');
 
-var foo = [ 'a', 'b', 'c' ];
-var foo = new Array('a', 'b', 'c');
-var foo = Array('a', 'b', 'c');
+// creating array
 
-console.log(foo[0]); // 'a'
-console.log(foo.length); // 3
+var a1 = new Array(3);
+var a2 = Array(3);
+var a3 = []; a3.length = 3;
 
-var foo = new Array(3);
-var foo = Array(3);
-var foo = []; foo.length = 3;
+assert.equal(a1[0], undefined);
+assert.equal(a1[1], undefined);
+assert.equal(a1[2], undefined);
+assert.equal(a1.length, 3);
 
-console.log(foo); // [ undefined, undefined, undefined, ]
+assert.deepEqual(a1, a2);
+assert.deepEqual(a2, a3);
+assert.notDeepEqual(a1, [ undefined, undefined, undefined ]);
 
-var foo = [ 'a', , 'c' ];
+assert.equal([ , , ].length, 2); // not 3
 
-console.log(foo); // ['a', undefined , 'c']
+var a1 = [ 'a', 'b', 'c' ];
+var a2 = new Array('a', 'b', 'c');
+var a3 = Array('a', 'b', 'c');
+
+assert.equal(a1[0], 'a');
+assert.equal(a1[1], 'b');
+assert.equal(a1[2], 'c');
+assert.equal(a1.length, 3);
+
+assert.deepEqual(a1, a2);
+assert.deepEqual(a2, a3);
+
+var a = [ 'a', , 'c' ];
+
+assert.equal(a[0], 'a');
+assert.equal(a[1], undefined);
+assert.equal(a[2], 'c');
+assert.equal(a.length, 3);
 
 
 // length
 
-var foo = [ 'a', 'b', 'c' ];
-console.log(foo.length); // 3
+var a = [ 'a', 'b', 'c' ];
 
-foo.length = 2;
-console.log(foo); // ['a', 'b']
+assert.equal(a.length, 3);
 
-foo.length = 0;
-console.log(foo); // []
+a.length = 2;
+assert.deepEqual(a, [ 'a', 'b' ]);
 
-foo.length = 3;
-console.log(foo); // [ undefined, undefined, undefined ]
+a.length = 0;
+assert.deepEqual(a, []);
+
+a.length = 3;
+assert.deepEqual(a, new Array(3));
 
 
 // enumerating
 
 var colors = [ 'red', 'green', 'blue' ];
 
+var r = [];
 for (var i = 0; i < colors.length; i++) {
-	console.log(colors[i]); // 'red', 'green', 'blue'
+	r.push(colors[i]);
 }
 
-colors.forEach(function(color) { // ECMAScript 5
-	console.log(color);  // 'red', 'green', 'blue'
+assert.deepEqual(r, [ 'red', 'green', 'blue' ]);
+
+var r = [];
+colors.forEach(function(color) {
+	r.push(color);
 });
 
-console.log([ 'a', 'b', 'c' ].map(function (item) {
-	return item.toUpperCase();
-})); // [ 'A', 'B', 'C' ]
+assert.deepEqual(r, [ 'red', 'green', 'blue' ]);
 
-console.log([ 'a', 10, 'b', 20 ].filter(function (item) {
+assert.deepEqual([ 'a', 'b', 'c' ].map(function (item) {
+	return item.toUpperCase();
+}), [ 'A', 'B', 'C' ]);
+
+assert.deepEqual([ 'a', 10, 'b', 20 ].filter(function (item) {
 	return typeof item === 'number';
-})); // [ 10, 20 ]
+}), [ 10, 20 ]);
 
 function isNumber(value){
 	return typeof value == 'number';
 }
 
-console.log([ 1, 2, 3 ].every(isNumber)); // true
-console.log([ 1, '2', 3 ].every(isNumber)); // false
+assert.equal([ 1, 2, 3 ].every(isNumber), true);
+assert.equal([ 1, '2', 3 ].every(isNumber), false);
 
-console.log([ 1, 2, 3 ].some(isNumber)); // true
-console.log([ 1, '2', 3 ].some(isNumber)); // true
-console.log([ '1', '2', '3' ].some(isNumber)); // false
+assert.equal([ 1, 2, 3 ].some(isNumber), true);
+assert.equal([ 1, '2', 3 ].some(isNumber), true);
+assert.equal([ '1', '2', '3' ].some(isNumber), false);
 
-console.log([ 1, 2, 3].reduce(function(first, second) {
+assert.equal([ 1, 2, 3].reduce(function(first, second) {
 	return first + second;
-}, 0)); // 6
+}, 0), 6);
 
-console.log([ 1, 2, 3].reduceRight(function(first, second) {
+assert.equal([ 1, 2, 3].reduceRight(function(first, second) {
 	return first + second;
-}, 0)); // 6
+}, 0), 6);
 
 
 // sorting
@@ -79,7 +105,7 @@ var arr = [ 52, 97, 3, 62, 10, 63, 64, 1, 9, 3, 4 ];
 
 arr.sort()
 
-console.log(arr); // [ 1, 10, 3, 3, 4, 52, 62, 63, 64, 9, 97 ]
+assert.deepEqual(arr, [ 1, 10, 3, 3, 4, 52, 62, 63, 64, 9, 97 ]);
 
 arr.sort(function(a,b) {
 	if (a < b) {
@@ -90,64 +116,67 @@ arr.sort(function(a,b) {
 	return 0;
 });
 
-console.log(arr); // [ 1, 3, 3, 4, 9, 10, 52, 62, 63, 64, 97 ]
+assert.deepEqual(arr, [ 1, 3, 3, 4, 9, 10, 52, 62, 63, 64, 97 ]);
 
-console.log([ 1, 2, 3 ].reverse()); // [ 3, 2, 1 ]
+assert.deepEqual([ 1, 2, 3 ].reverse(), [ 3, 2, 1 ]);
 
 
 // push, pop
 
-var ary = [ 1, 2, 3 ];
+var a = [ 1, 2, 3 ];
 
-ary.push(10);
-console.log(ary); // [ 1, 2, 3, 10 ]
+a.push(10);
+assert.deepEqual(a, [ 1, 2, 3, 10 ]);
 
-console.log(ary.pop()); // 10
-console.log(ary); // [ 1, 2, 3 ]
+assert.equal(a.pop(), 10);
+assert.deepEqual(a, [ 1, 2, 3 ]);
 
-var ary = [ 1, 2, 3 ];
+var a = [ 1, 2, 3 ];
 
-console.log(ary.shift()); // 1
-console.log(ary); // [ 2, 3 ]
+assert.deepEqual(a.shift(), 1);
+assert.deepEqual(a, [ 2, 3 ]);
 
-var ary = [ 1, 2, 3 ];
+var a = [ 1, 2, 3 ];
 
-console.log(ary.unshift(99)); // length, 4
-console.log(ary); // [ 99, 1, 2, 3 ]
+a.unshift(99)
+assert.deepEqual(a, [ 99, 1, 2, 3 ]);
 
 
 // concat, slice
 
-console.log([ 1, 2, 9 ].concat(4, 5)); // [ 1, 2, 9, 4, 5 ]
-console.log([ 1, 2, 9 ].concat([ 4, 5 ])); // [ 1, 2, 9, 4, 5 ]
-console.log([ 1, 2, 9 ].concat([ 4, 5 ], [ 6, 7 ])); // [ 1, 2, 9, 4, 5, 6, 7 ]
+var a = [ 1, 2, 9 ];
 
-console.log([ 1, 2, 3].join('-')); // '1-2-3'
+assert.deepEqual(a.concat(4, 5), [ 1, 2, 9, 4, 5 ]);
+assert.deepEqual(a.concat([ 4, 5 ]), [ 1, 2, 9, 4, 5 ]);
+assert.deepEqual(a.concat([ 4, 5 ], [ 6, 7 ]), [ 1, 2, 9, 4, 5, 6, 7 ]);
 
-var ary = [ 'a', 'b', 'c', 'd', 'e' ];
+assert.deepEqual(a.join('-'), '1-2-9');
 
-console.log(ary.slice(1, 3)); // [ 'b', 'c' ]
-console.log(ary); // [ 'a', 'b', 'c', 'd', 'e' ]
+var a = [ '0', '1', '2', '3', '4' ];
 
-var ary = [ 'a', 'b', 'c', 'd', 'e' ];
+assert.deepEqual(a.slice(1, 3), [ '1', '2' ]);
+assert.deepEqual(a, [ '0', '1', '2', '3', '4' ]);
 
-console.log(ary.splice(1, 3)); // [ 'b', 'c', 'd' ]
-console.log(ary); // [ 'a', 'e' ]
+var a = [ '0', '1', '2', '3', '4' ];
 
-var ary = [ 'a', 'b', 'c', 'd', 'e' ];
+assert.deepEqual(a.splice(1, 3), [ '1', '2', '3' ]);
+assert.deepEqual(a, [ '0', '4' ]);
 
-console.log(ary.splice(1, 3, 77, 99)); // [ 'b', 'c', 'd' ]
-console.log(ary); // [ 'a', 77, 99, 'e' ]
+var a = [ '0', '1', '2', '3', '4' ];
 
-var ary = [ 'a', 'b', 'c', 'd', 'e' ];
+assert.deepEqual(a.splice(1, 3, 77, 99), [ '1', '2', '3' ]);
+assert.deepEqual(a, [ '0', 77, 99, '4' ]);
 
-console.log(ary.splice(1, 3, [ 77, 99 ])); // [ 'b', 'c', 'd' ]
-console.log(ary); // [ 'a', [ 77, 99 ], 'e' ]
+var a = [ '0', '1', '2', '3', '4' ];
+
+assert.deepEqual(a.splice(1, 3, [ 77, 99 ]), [ '1', '2', '3' ]);
+assert.deepEqual(a, [ '0', [ 77, 99 ], '4' ]);
+
 
 // searching
 
-console.log([ 'a', 'b', 'a', 'b', 'a' ].indexOf('b')); // 1
-console.log([ 'a', 'b', 'a', 'b', 'a' ].indexOf('b', 2)); // 3
-console.log([ 'a', 'b', 'a', 'b', 'a' ].indexOf('z')); // -1
+assert.equal([ 'a', 'b', 'a', 'b', 'a' ].indexOf('b'), 1);
+assert.equal([ 'a', 'b', 'a', 'b', 'a' ].indexOf('b', 2), 3);
+assert.equal([ 'a', 'b', 'a', 'b', 'a' ].indexOf('z'), -1);
 
-console.log([ 'a', 'b', 'a', 'b', 'a' ].lastIndexOf('b')); // 3
+assert.equal([ 'a', 'b', 'a', 'b', 'a' ].lastIndexOf('b'), 3);
