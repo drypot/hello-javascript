dd46d70f6826c4e799cd1737f5b9f6885fd8943a
drypot <drypot@gmail.com>
2013-03-20 16:14:20 +0900

    updated number.js.

--- javascript-abc/number.js
+++ javascript-abc/number.js
@@ -1,25 +1,37 @@
+var assert = require('assert');
 
+assert.equal(typeof 0, 'number');        // there is no integer type.
+assert.equal(typeof 42, 'number');       // always 64bit floating point.
+assert.equal(typeof 3.14, 'number');
+assert.equal(typeof 1e2, 'number');      // 100
+assert.equal(typeof NaN, 'number');      // global.NaN
+assert.equal(typeof Infinity, 'number'); // global.Infinity, greater than 1.79769313486231570e+308
 
-console.log(0);        // there is no integer type.
-console.log(42);       // always 64bit floating point.
-console.log(3.14159);
-console.log(1e2);      // 100
+assert.equal(10, 10);   // base 10
+assert.equal(010, 8);   // base 8
+assert.equal(0x10, 16); // base 16
 
-console.log(10);       // base 10
-console.log(070);      // base 8
-console.log(0x1123);   // base 16
+assert.equal(parseInt('11'), 11);
+assert.equal(parseInt('11.1'), 11);
+assert.equal(parseInt('11xx'), 11);
+assert.equal(isNaN(parseInt('xx')), true);
+assert.equal(isNaN(parseInt('')), true);
 
-console.log(NaN);      // global.NaN
-console.log(Infinity); // global.Infinity, greater than 1.79769313486231570e+308
+assert.equal(parseFloat('11.1'), 11.1);
 
-console.log(parseInt('11'));   // 11
-console.log(parseInt('11.1')); // 11
-console.log(parseInt('11xx')); // 11
-console.log(parseInt('xx'));   // NaN
-console.log(parseInt(''));     // NaN
+assert.equal(typeof '11', 'string');
+assert.equal(typeof +'11', 'number');
 
-console.log(parseFloat('11.1')); // 11.1
+assert.equal(typeof (84.5283).toFixed(0), 'string'); //
+assert.equal((84.5283).toFixed(0), '85');
+assert.equal((84.5283).toFixed(2), '84.53');
 
-console.log(typeof '11');   // 'string'
-console.log(typeof +'11');  // 'number'
+assert.equal(typeof (84.5283).toPrecision(1), 'string');
+assert.equal((84.5283).toPrecision(1), '8e+1');
+assert.equal((84.5283).toPrecision(2), '85');
+assert.equal((84.5283).toPrecision(4), '84.53');
 
+assert.equal((123).toString(), '123');
+assert.equal((16).toString(16), '10'); // radix
+
+assert.equal((123).toString(), String(123));
